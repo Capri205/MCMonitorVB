4b396d90455a16663dd07b2cd550a347695863d5
Fahrenheit451 <sean451@hotmail.com>
2021-03-25 10:12:06 -0400

    use https from now on

--- MCMonitorVB/Controllers/HomeController.vb
+++ MCMonitorVB/Controllers/HomeController.vb
@@ -251,14 +251,13 @@ Public Class HomeController
         databytestr = databytestr + "&ip=" & ipaddr & "&port=" & CStr(port)
         Dim byteData As Byte() = encoding.GetBytes(databytestr)
         Dim postReq As HttpWebRequest
-        'postReq = DirectCast(WebRequest.Create("https://ob-mc.net/serverquery/query.php"), HttpWebRequest)
-        postReq = DirectCast(WebRequest.Create("http://192.168.1.52:80/serverquery/query.php"), HttpWebRequest)
+
+        postReq = DirectCast(WebRequest.Create("https://ob-mc.net/serverquery/query.php"), HttpWebRequest)
         postReq.Method = "POST"
         postReq.KeepAlive = True
         postReq.ContentType = "application/x-www-form-urlencoded"
         'postReq.CookieContainer = tempCookies
-        'postReq.Referer = "https://ob-mc.net/serverquery/query.php"
-        postReq.Referer = "http://192.168.1.52:80/serverquery/query.php"
+        postReq.Referer = "https://ob-mc.net/serverquery/query.php"
         'postReq.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64, rv:26.0) Gecko/20100101 Firefox/26.0"
         postReq.ContentLength = byteData.Length
 
@@ -282,14 +281,13 @@ Public Class HomeController
         databytestr = databytestr + "&id=" & qid
         Dim byteData As Byte() = encoding.GetBytes(databytestr)
         Dim postReq As HttpWebRequest
-        'postReq = DirectCast(WebRequest.Create("https://ob-mc.net/serverquery/puquery.php"), HttpWebRequest)
-        postReq = DirectCast(WebRequest.Create("http://192.168.1.52/serverquery/puquery.php"), HttpWebRequest)
+
+        postReq = DirectCast(WebRequest.Create("https://ob-mc.net/serverquery/puquery.php"), HttpWebRequest)
         postReq.Method = "POST"
         postReq.KeepAlive = True
         postReq.ContentType = "application/x-www-form-urlencoded"
         'postReq.CookieContainer = tempCookies
-        'postReq.Referer = "https://ob-mc.net/serverquery/puquery.php"
-        postReq.Referer = "http://192.168.1.52/serverquery/puquery.php"
+        postReq.Referer = "https://ob-mc.net/serverquery:80/puquery.php"
         'postReq.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64, rv:26.0) Gecko/20100101 Firefox/26.0"
         postReq.ContentLength = byteData.Length
 
